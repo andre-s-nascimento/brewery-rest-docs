620f2fa18dd3342ec17536f0c6f9989d687131f7
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-30 10:12:50 -0300

    Final Tarefa

--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -1,26 +1,23 @@
 package guru.springframework.msscbrewery.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import java.util.UUID;
-
-/**
- * Created by jt on 2019-04-21.
- */
+/** Created by jt on 2019-04-21. */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CustomerDto {
 
-    private UUID id;
+  private UUID id;
 
-    @NotBlank
-    @Size(min = 3, max = 100)
-    private String name;
+  @NotBlank
+  @Size(min = 3, max = 100)
+  private String name;
 }
